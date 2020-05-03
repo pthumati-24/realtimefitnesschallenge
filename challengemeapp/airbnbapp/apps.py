ff4a69add3bb502f522680aa993216d3ffbd2777
Sai Hemanth Gantasala <gsaihemanth619@gmail.com>
2020-05-02 22:59:02 -0700

    webrtc

--- challengemeapp/airbnbapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AirbnbappConfig(AppConfig):
-    name = 'airbnbapp'
--- challengemeapp/moviesapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MoviesappConfig(AppConfig):
-    name = 'moviesapp'
